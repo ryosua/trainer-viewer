7292bf9a3e89c9bdeeecbf8c910897ec21eda74c
ryosua <hi@ryanyosua.me>
2019-07-27 10:49:01 -0600

    Copy changes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View, Platform } from 'react-native'
+import { Text, Platform } from 'react-native'
+import styled from 'styled-components/native'
+
+const StyledView = styled.View`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`
 
 class App extends Component {
     render() {
         return (
-            <View>
-                <Text>Get Stoked App</Text>
-            </View>
+            <StyledView>
+                <Text>Need to find a route? Get Stoked!</Text>
+                <br />
+                <Text>
+                    Stoked will be a new climbing guide that will help you find new routes faster, and spend more time
+                    climbing.
+                </Text>
+                <br />
+                <Text>Buy a T-Shirt to get access to the beta for a year when it is ready.</Text>
+            </StyledView>
         )
     }
 }
