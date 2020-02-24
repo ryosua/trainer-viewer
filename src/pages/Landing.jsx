af8e21cae5b0decf1e8c99924dd1026a064e05f6
ryosua <hi@ryanyosua.me>
2020-02-23 20:54:31 -0700

    basic landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    root: {
+        width: 200
+    }
+})
+
+const Landing = () => {
+    const classes = useStyles()
+    return (
+        <Box display="flex" flex={1} flexDirection="column" justifyContent="center">
+            <Typography variant="h3">Stoked</Typography>
+            <Typography>Meet people while exploring the outdoors.</Typography>
+            <Button classes={classes}>Signup</Button>
+        </Box>
+    )
+}
+
+export default Landing
