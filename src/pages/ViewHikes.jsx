1fcb521ce30f4d9fd7c9b1899d3f5a0f443edbfb
ryosua <hi@ryanyosua.me>
2020-03-08 12:45:45 -0600

    updated trail info

--- src/pages/ViewHikes.jsx
+++ src/pages/ViewHikes.jsx
@@ -7,18 +7,26 @@ import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 
-const VIEW_HIKES_QUERY = gql`
-    {
+const ViewHikesQuery = gql`
+    query ViewHikes {
         hikes {
             id
+            carpoolOptions
+            elevationGain
+            expectedRoundTripTime
             miles
+            name
+            parkingLocation
+            preparationNotes
+            startingElevation
+            time
             url
         }
     }
 `
 
 const ViewHikes = () => {
-    const { loading, error, data } = useQuery(VIEW_HIKES_QUERY)
+    const { loading, error, data } = useQuery(ViewHikesQuery)
     if (loading || error) {
         return null
     }
@@ -26,19 +34,42 @@ const ViewHikes = () => {
     return (
         <>
             <Typography variant="h2">View Hikes</Typography>
-            {hikes.map(({ id, miles, url }) => (
-                <Card key={id} variant="outlined">
-                    <CardContent>
-                        <Typography>{`Link: ${url}`}</Typography>
-                        <Typography>{`Miles: ${miles}`}</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" onClick={() => window.open(url, '_blank')}>
-                            Show Details
-                        </Button>
-                    </CardActions>
-                </Card>
-            ))}
+            {hikes.map(
+                ({
+                    id,
+                    carpoolOptions,
+                    elevationGain,
+                    expectedRoundTripTime,
+                    miles,
+                    name,
+                    parkingLocation,
+                    preparationNotes,
+                    startingElevation,
+                    time,
+                    url
+                }) => (
+                    <Card key={id} variant="outlined">
+                        <CardContent>
+                            <Typography variant="h3">{name}</Typography>
+                            <Typography>{`Miles: ${miles}`}</Typography>
+                            <Typography>{`Date and Time: ${time}`}</Typography>
+                            <Typography>{`Starting Elevation: ${startingElevation} feet`}</Typography>
+                            <Typography>{`Elevation Gain: ${elevationGain} feet`}</Typography>
+                            <Typography>{`Expected Round Trip Time: ${expectedRoundTripTime / 60} hours`}</Typography>
+                            <Typography>{`Carpool Options: ${carpoolOptions}`}</Typography>
+                            <Typography>{`Preperation Notes: ${preparationNotes}`}</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" onClick={() => window.open(url, '_blank')}>
+                                View on All Trails
+                            </Button>
+                            <Button size="small" onClick={() => window.open(parkingLocation, '_blank')}>
+                                Parking Location
+                            </Button>
+                        </CardActions>
+                    </Card>
+                )
+            )}
         </>
     )
 }
