9d72bae9c566aeb772177a081c56f66d1c75d1db
ryosua <hi@ryanyosua.me>
2020-03-14 17:37:38 -0600

    add token to graphql requests and clear on logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Box from '@material-ui/core/Box'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { useAuth0 } from './Auth0'
 
 import NavBar from './components/NavBar'
 import ViewHikes from './pages/ViewHikes'
@@ -11,23 +12,47 @@ import Landing from './pages/Landing'
 import './App.css'
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_GRAPHQL_API
+    uri: process.env.REACT_APP_GRAPHQL_API,
+    request: operation => {
+        const token = localStorage.getItem('token')
+        operation.setContext({
+            headers: {
+                authorization: token ? token : ''
+            }
+        })
+    }
 })
 
-const App = () => (
-    <div className="App">
-        <ApolloProvider client={client}>
-            <Router>
-                <header>
-                    <NavBar />
-                </header>
-                <Box display="flex" flexDirection="column" mt={2}>
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/hikes" component={ViewHikes} />
-                </Box>
-            </Router>
-        </ApolloProvider>
-    </div>
-)
+const App = () => {
+    const auth0 = useAuth0()
+    const isAuthenticated = auth0.isAuthenticated
+
+    useEffect(() => {
+        const getToken = async () => {
+            const tokenClaims = await auth0.getIdTokenClaims()
+            const token = tokenClaims.__raw
+            localStorage.setItem('token', token)
+        }
+        if (isAuthenticated) {
+            getToken()
+        }
+    }, [auth0, isAuthenticated])
+
+    return (
+        <div className="App">
+            <ApolloProvider client={client}>
+                <Router>
+                    <header>
+                        <NavBar />
+                    </header>
+                    <Box display="flex" flexDirection="column" mt={2}>
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/hikes" component={ViewHikes} />
+                    </Box>
+                </Router>
+            </ApolloProvider>
+        </div>
+    )
+}
 
 export default App
--- src/Auth0.jsx
+++ src/Auth0.jsx
@@ -72,10 +72,12 @@ export const Auth0Provider = ({ children, onRedirectCallback = DEFAULT_REDIRECT_
                 loginWithRedirect: (...p) => auth0Client.loginWithRedirect(...p),
                 getTokenSilently: (...p) => auth0Client.getTokenSilently(...p),
                 getTokenWithPopup: (...p) => auth0Client.getTokenWithPopup(...p),
-                logout: (...p) =>
+                logout: (...p) => {
                     auth0Client.logout({
                         returnTo: `http://${process.env.REACT_APP_URL}`
                     })
+                    localStorage.setItem('token', '')
+                }
             }}>
             {children}
         </Auth0Context.Provider>
