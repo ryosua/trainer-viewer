973f2da31cc48e13b69b6ffc6ff9d162fbe88a6c
ryosua <hi@ryanyosua.me>
2020-03-29 16:04:44 -0600

    add basic form

--- src/pages/CreateWorkout.jsx
+++ src/pages/CreateWorkout.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
-const CreateWorkout = () => <Typography variant="h2">Create Workout</Typography>
+const CreateWorkout = () => (
+    <>
+        <Typography variant="h2">Create Workout</Typography>
+        <TextField label={'Title'} />
+        <TextField label={'Link'} />
+        <Button>Create Workout</Button>
+    </>
+)
 
 export default CreateWorkout
