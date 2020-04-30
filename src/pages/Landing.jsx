70feba9e290fef000e87176451a9b25c6671d031
ryosua <hi@ryanyosua.me>
2020-04-29 19:03:06 -0600

    added breakpoint for mobile

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/core/styles'
 
 import Button from '../components/Button'
 import useAuth from '../hooks/useAuth'
@@ -8,18 +9,39 @@ import analytics from '../utils/analytics'
 
 import { ReactComponent as Logo } from '../assets/logo.svg'
 
+const useStyles = makeStyles((theme) => ({
+    logo: {
+        [theme.breakpoints.down('sm')]: {
+            width: 200
+        },
+        [theme.breakpoints.up('md')]: {
+            width: 350
+        }
+    },
+    video: {
+        [theme.breakpoints.down('sm')]: {
+            width: 280,
+            height: 157.5
+        },
+        [theme.breakpoints.up('md')]: {
+            width: 560,
+            height: 315
+        }
+    }
+}))
+
 const Landing = () => {
+    const classes = useStyles()
     const { isAuthenticated } = useToken()
     const { loginWithRedirect } = useAuth()
     return (
         <Box display="flex" flex={1} justifyContent="center">
             <Box display="flex" flexDirection="column" alignItems="center">
-                <Logo />
+                <Logo className={classes.logo} />
                 <Box m={5}>
                     <iframe
+                        className={classes.video}
                         title="video"
-                        width="560px"
-                        height="315px"
                         src="https://www.youtube.com/embed/LiRHH3-yk8g"
                         frameBorder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
